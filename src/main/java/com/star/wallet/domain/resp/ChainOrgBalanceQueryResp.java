b9e3224fc3f814230a783add9758a2462639f365
666 <1@gmail.com>
2025-09-12 17:57:27 +0800

    feat:參數修改

--- src/main/java/com/star/wallet/domain/resp/ChainOrgBalanceQueryResp.java
+++ src/main/java/com/star/wallet/domain/resp/ChainOrgBalanceQueryResp.java
@@ -1,5 +1,6 @@
 package com.star.wallet.domain.resp;
 
+import com.star.wallet.domain.common.BaseDto;
 import lombok.Data;
 
 import com.star.wallet.domain.common.ChainOrgBalanceDto;
@@ -8,19 +9,12 @@ import java.util.List;
 
 //商户钱包余额查询响应
 @Data
-public class ChainOrgBalanceQueryResp implements Serializable {
+public class ChainOrgBalanceQueryResp extends BaseDto implements Serializable {
 
     //商户钱包余额
     private List<ChainOrgBalanceDto> coinBalanceList;
 
     //状态1正常0禁用
     private Integer status;
-
-    //时间戳 毫秒
-    private Long timestamp;
-
-
-
-
 
 }
